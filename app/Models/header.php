f424d56e5955b001d6095f8ac8ed7f1318fbc753
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-11-27 20:00:36 -0300

    create header and footer models

---
+++ app/Models/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="">
+  <script src="https://unpkg.com/scrollreveal"></script>
+  <title>Livro de Visitas</title>
+</head>
+
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="" id="linkHome">Cadastrar</a></li>
+        <li><a href="" id="linkLista"> Listar assinaturas</a></li>
+      </ul>
+    </nav>
+  </header>
+  <h1 id="h1">Header</h1>
