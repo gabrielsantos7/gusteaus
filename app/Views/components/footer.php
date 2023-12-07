d177c5dc905106bbc0b2abe02d429e42e5a6f909
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-12-07 14:12:27 -0300

    corrected footer icons

--- app/Views/components/footer.php
+++ app/Views/components/footer.php
@@ -1,18 +1,18 @@
 <footer id="footer">
-    <div class="container">
-      <h3>Gusteau’s</h3>
-      <p>Conheça nossas redes sociais</p>
-      <div class="social-links">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-      </div>
-      <div class="copyright">
-        &copy; Copyright <strong><span>Gusteau’s</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-      </div>
+  <div class="container">
+    <h3>Gusteau’s</h3>
+    <p>Conheça nossas redes sociais</p>
+    <div class="social-links">
+      <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
+      <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+      <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
     </div>
-</body>
+    <div class="copyright">
+      &copy; Copyright 2023 <span class="fw-bold">Gusteau’s</span>. All Rights Reserved
+    </div>
+    <div class="credits">
+    </div>
+  </div>
+  </body>
 
-</html>
+  </html>
