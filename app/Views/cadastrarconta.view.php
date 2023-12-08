81bee0591dd5c7fbdec37d14e6bd942c9e63290d
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-12-08 11:28:58 -0300

    create cadastrarconta basic style

--- app/Views/cadastrarconta.view.php
+++ app/Views/cadastrarconta.view.php
@@ -1,13 +1,15 @@
 <?php
 $cssPath = PUBLIC_PATH . 'css/cadastrarconta.css';
-$title = 'Cadastrar Conta';
+$title = 'Sucesso!';
 
 require VIEW_PATH .  'components/header.php'
 ?>
 
 <main>
-  <div class="container bg-secondary">
-    <h2 class="fw-bold text-yellow">Sucesso ao criar conta!</h2>
+  <div class="container text-center">
+    <h2 class="fw-bold text-yellow my-5">Sucesso ao criar conta!</h2>
+    <p>Parabéns! Sua conta foi criada com sucesso! Agora você pode desfrutar de todas as funcionalidades oferecidas. Faça pedidos, reserve mesas e aproveite ao máximo a sua experiência conosco. Bem-vindo!</p>
+    <a href="/gusteaus/cardapio" class="btn btn-primary my-5">Fazer pedido</a>
   </div>
 </main>
 
